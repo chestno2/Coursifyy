31d2e6ed839e95860bd18bf5be46687537b4cb17
Atul <atuly152001@gmail.com>
2020-11-20 13:29:13 +0530

    2 nd attempt

---
+++ index.js
@@ -0,0 +1,47 @@
+$('.cara').slick({
+    dots:true,
+    autoplay:true,
+    autoplaySpeed: 2000,
+});
+
+
+$('.cara2').slick({
+    dots:true,
+    autoplay:true,
+    autoplaySpeed: 2000,
+});
+
+$('.cara3').slick({
+    dots:true,
+    autoplay:true,
+    autoplaySpeed: 2000,
+});
+
+
+$(document).ready(function()
+{
+  $('#icon').click(function()
+  {
+    $("#menu-small").slideToggle("fast");
+  })
+});
+
+
+$(document).ready(function()
+{
+  $('#button').click(function()
+  {
+    $("#expand").slideToggle("fast");
+  })
+});
+
+
+$(document).ready(function()
+{
+  $('#button1').click(function()
+  {
+    $("#expand1").slideToggle("fast");
+  })
+});
+
+
